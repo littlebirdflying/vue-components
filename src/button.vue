8c53e235f1c2887c919a186c10f3e978a4797098
littlebirdflying <tongmengwang@163.com>
2019-11-17 16:24:51 +0800

    button组件：添加loading状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="m-button" :class="{[`icon-${iconPosition}`]: true}">
         <m-icon class="icon" v-if="icon" :name="icon"></m-icon>
+        <m-icon class="loading" name="loading"></m-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -22,6 +23,10 @@
     }
 </script>
 <style lang="scss">
+    @keyframes spin {
+        0% {transform: rotate(0deg)}
+        100% {transform: rotate(360deg)}
+    }
     .m-button {
         font-size: var(--font-size);
         height: var(--button-height);
@@ -58,6 +63,9 @@
             > .content {
                 order: 1;
             }
+        }
+        .loading {
+            animation: spin 2s infinite linear;
         }
     }
 
